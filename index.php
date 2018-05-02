1c3ee8d2a7b412c1dd1375ed88844e045269fe7d
Jessie Calipara Jamola <jessie.kun8@gmail.com>
2018-05-02 13:32:58 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Project-IB - Main Menu</title>
+	<!-- Style -->
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="admin/assets/css/bootstrap.min.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="admin/assets/css/bootstrap-theme.min.css">
+
+	<link rel="stylesheet" href="admin/assets/css/login.css">
+	<link rel="stylesheet" href="admin/assets/css/jquery-confirm.css">
+	<!-- <link rel="stylesheet" href="organize/assets/css/header.css">
+	<link rel="stylesheet" href="organize/assets/css/footer.css"> -->
+
+	<style type="text/css">
+		/*input {
+			font-size: 18px !important;
+			padding: 20px !important;
+		}*/
+	</style>
+
+	
+</head>
+<body>
+	<?php include_once('admin/views/partials/header.php'); ?>
+	<div class="container">
+	  <!-- <h1>Attendance Checker - Main Menu</h1> -->
+	  <div class="row">
+	  	<a href="admin/index.php"><div class="col-md-12 text-center" style="padding: 5em;">
+		  	<h1 style="font-weight: 800;">Administrator</h1>
+		  </div></a>
+
+		  <a href="customer/index.php"><div class="col-md-12 text-center" style="padding: 5em;">
+		  	<h1 style="font-weight: 800;">Customer</h1>
+		  </div></a>
+	  </div>
+	</div>
+</body>
+</html>
